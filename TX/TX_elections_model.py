0efa926fee7c5ceafcf8679633d643657e6d6c9e
daragold <daracaseygold@gmail.com>
2021-01-05 18:41:48 -0500

    comment in graph

--- TX/TX_elections_model.py
+++ TX/TX_elections_model.py
@@ -125,14 +125,14 @@ state_df = pd.DataFrame(state_gdf)
 state_df = state_df.drop(['geometry'], axis = 1)
 
 #build graph from geo_dataframe #####################################################
-#graph = Graph.from_geodataframe(state_gdf)
-#graph.add_data(state_gdf)
-#centroids = state_gdf.centroid
-#c_x = centroids.x
-#c_y = centroids.y
-#for node in graph.nodes():
-#    graph.nodes[node]["C_X"] = c_x[node]
-#    graph.nodes[node]["C_Y"] = c_y[node]
+graph = Graph.from_geodataframe(state_gdf)
+graph.add_data(state_gdf)
+centroids = state_gdf.centroid
+c_x = centroids.x
+c_y = centroids.y
+for node in graph.nodes():
+    graph.nodes[node]["C_X"] = c_x[node]
+    graph.nodes[node]["C_Y"] = c_y[node]
     
 #set up elections data structures ################################################
 elections = list(elec_data["Election"]) 
